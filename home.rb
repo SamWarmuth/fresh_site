084c073f311ed773da823665abb2de377a1fe4bc
Sam Warmuth <samwarmuth@sam-warmuths-macbook-pro.local>
2009-11-30 14:14:31 -0600

    added 'last week' section, made section headings smaller

--- home.rb
+++ home.rb
@@ -5,7 +5,7 @@ require 'sass'
 require 'open-uri'
 require 'rss'
 before do headers "Content-Type" => "text/html; charset=utf-8" end
-$content = {:today => [], :yesterday => [], :earlier =>[]}
+$content = {}
 
 
 get '/' do 
@@ -18,19 +18,22 @@ get '/:page.css' do
 end
 
 def load_data()
-	$content = {:today => [], :yesterday => [], :earlier =>[]}
+	$content = {:today => [], :yesterday => [], :last_week => [], :earlier =>[]}
 	content = ''
 	[["","http://www.instapaper.com/rss/2588/CUrq0Y7Vt5yZKxhIOrDqGWxLk"],["GSR","http://www.givemesomethingtoread.com/rss"]].each do |url|
 		open(url[1]) {|source| content = source.read}
 		rss = RSS::Parser.parse(content, false)
 		rss.items.each do |item|
 			if item.date.yday == Time.now.yday
-				$content[:today] << {:src => url[0], :title => item.title, :url => item.link, :guid => item.guid.to_s.gsub(/<\/?guid>/, ''), :description => item.comments}
+				section = :today
 			elsif item.date.yday == (Time.now.yday - 1)
-				$content[:yesterday] << {:src => url[0], :title => item.title, :url => item.link, :guid => item.guid.to_s.gsub(/<\/?guid>/, ''), :description => item.comments}
+				section = :yesterday
+			elsif Time.now.yday - item.date.yday < 7
+				section = :last_week
 			else
-				$content[:earlier] << {:src => url[0], :title => item.title, :url => item.link, :guid => item.guid.to_s.gsub(/<\/?guid>/, ''), :description => item.comments}
+				section = :earlier
 			end
+			$content[section] << {:src => url[0], :title => item.title, :url => item.link, :guid => item.guid.to_s.gsub(/<\/?guid>/, ''), :description => item.comments}
 		end
 	end
 end
