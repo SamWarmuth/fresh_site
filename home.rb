5a4364fb49cd5193f3498552aca8ef7c226b5fee
Sam Warmuth <samwarmuth@sam-warmuths-macbook-pro.local>
2009-12-01 01:51:00 -0600

    added pdb to rss list

--- home.rb
+++ home.rb
@@ -16,6 +16,13 @@ get '/fresh' do
 	load_data
 	haml :fresh
 end
+post '/tumblrpost' do
+	params['email']="sam@samwarmuth.com"
+	params['password']="calculo"
+	params['format']="markdown"
+	params['type']="regular"
+	redirect 'http://www.tumblr.com/api/write', 307
+end
 get '/:page.css' do
    content_type 'text/css', :charset => 'utf-8'
    sass params[:page].to_sym
@@ -32,7 +39,7 @@ end
 def load_data()
 	$content = {:today => [], :yesterday => [], :last_week => [], :earlier =>[]}
 	content = ''
-	[["","http://www.instapaper.com/rss/2588/CUrq0Y7Vt5yZKxhIOrDqGWxLk"],["GSR","http://www.givemesomethingtoread.com/rss"]].each do |url|
+	[["SW","http://pdb.samwarmuth.com/posts.xml"],["TBL","http://blog.samwarmuth.com/rss"],["INS","http://www.instapaper.com/rss/2588/CUrq0Y7Vt5yZKxhIOrDqGWxLk"],["GSR","http://www.givemesomethingtoread.com/rss"]].each do |url|
 		open(url[1]) {|source| content = source.read}
 		rss = RSS::Parser.parse(content, false)
 		rss.items.each do |item|
@@ -46,7 +53,7 @@ def load_data()
 			elsif Time.now.yday - item.date.yday < 30
 				section = :earlier
 			end
-			$content[section] << {:src => url[0], :title => item.title, :url => item.link, :guid => item.guid.to_s.gsub(/<\/?guid>/, ''), :description => item.comments} if section
+			$content[section] << {:src => url[0], :title => item.title, :url => item.link, :guid => item.guid.to_s.gsub(/<\/?guid>/, ''), :description => item.description.gsub(/<\/?blockquote>/, '')} if section
 		end
 	end
 end
