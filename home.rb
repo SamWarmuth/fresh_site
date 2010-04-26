612291f5d8e69b257f3587a2ad9f1b1a1de06c59
Sam Warmuth <samwarmuth@sam-warmuths-macbook-pro.local>
2010-04-26 00:17:59 -0500

    various stuff, mostly removed blog.samwarmuth and made / redirect to /blog

--- home.rb
+++ home.rb
@@ -9,7 +9,7 @@ $content = {}
 
 
 get '/' do 
-	redirect '/fresh'
+	redirect '/blog'
 end
 get '/fresh' do
 	params[:page] = 'Fresh'
@@ -28,30 +28,46 @@ get '/about' do
 	params[:page] = 'About'
 	haml :about
 end
+get '/blog' do
+  load_blog
+  params[:page] = 'Blog'
+  haml :blog
+end
 
 def load_data()
 	$content = {:today => [], :yesterday => [], :last_week => [], :earlier =>[]}
 	content = ''
 	[["SW","http://pdb.samwarmuth.com/posts.xml"],["TBL","http://blog.samwarmuth.com/rss"],["INS","http://www.instapaper.com/rss/2588/loMH7cAuH9BmL16bOe2G4wXht0"],["GSR","http://www.givemesomethingtoread.com/rss"]].each do |url|
-		begin
-			open(url[1]) {|source| content = source.read}
-		 rescue OpenURI::HTTPError
-		   next
-		 end
-		rss = RSS::Parser.parse(content, false)
-		next if rss.nil?
-		rss.items.each do |item|
-			next if Time.now.year != item.date.year
-			if item.date.yday == Time.now.yday
-				section = :today
-			elsif item.date.yday == (Time.now.yday - 1)
-				section = :yesterday
-			elsif Time.now.yday - item.date.yday < 7
-				section = :last_week
-			elsif Time.now.yday - item.date.yday < 30
-				section = :earlier
-			end
-			$content[section] << {:src => url[0], :title => item.title, :url => item.link, :guid => item.guid.to_s.gsub(/<\/?guid>/, ''), :description => item.description.to_s.gsub(/<\/?blockquote>/, '')} if section
-		end
-	end
+    begin
+      open(url[1]) {|source| content = source.read}
+    rescue OpenURI::HTTPError
+       next
+     end
+    rss = RSS::Parser.parse(content, false)
+    next if rss.nil?
+    rss.items.each do |item|
+      next if Time.now.year != item.date.year
+      if item.date.yday == Time.now.yday
+        section = :today
+      elsif item.date.yday == (Time.now.yday - 1)
+        section = :yesterday
+      elsif Time.now.yday - item.date.yday < 7
+        section = :last_week
+      elsif Time.now.yday - item.date.yday < 30
+        section = :earlier
+      end
+      $content[section] << {:src => url[0], :title => item.title, :url => item.link, :guid => item.guid.to_s.gsub(/<\/?guid>/, ''), :description => item.description.to_s.gsub(/<\/?blockquote>/, '')} if section
+    end
+  end
+end
+
+def load_blog
+  begin
+    content = ''
+    open("http://blog.samwarmuth.com/rss") {|source| content = source.read}
+    rss = RSS::Parser.parse(content, false)
+    @content = rss.items
+  rescue
+    @content = "Error Loading Blog."
+  end
 end
